fdfb985a0088b41c75729b2d22832636e4ce2013
Daniel-1017 <frimudaniel17@gmail.com>
2025-10-21 23:14:15 +0200

    Display workouts table

--- frontend/app/routes/topics/sport.tsx
+++ frontend/app/routes/topics/sport.tsx
@@ -1,27 +1,43 @@
-import { Box, Container } from "@radix-ui/themes";
+import { Button, Container, Table } from "@radix-ui/themes";
 import { useLoaderData } from "react-router";
-import Exercises from "~/components/Exercises";
 
 const Sport = () => {
-  const data = useLoaderData();
+  const { data } = useLoaderData();
+  const workouts: Workout[] = data.content;
 
   return (
-    <Box>
+    <section>
       <Container>
-        <div className="grid grid-cols-2">
-          <div>
-            List
-          </div>
+        <Button>New Workout</Button>
 
-          {data.data?.content?.length > 0 && <Exercises exercises={data.data.content} />}
-        </div>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>#</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Notes</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Last updated</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {workouts.map((workout, idx) => (
+              <Table.Row key={workout.id}>
+                <Table.RowHeaderCell>{idx}</Table.RowHeaderCell>
+                <Table.Cell>{workout.name}</Table.Cell>
+                <Table.Cell>{workout.notes}</Table.Cell>
+                <Table.Cell>{new Date(workout.updated).toLocaleString()}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
       </Container>
-    </Box>
+    </section>
   );
 };
 
 export const loader = async () => {
-  const response = await fetch("http://localhost:8090/api/v1/exercises?page=0&size=10&sort=name,asc");
+  const response = await fetch("http://localhost:8090/api/v1/workouts?page=0&size=10&sort=name,asc");
   const data = await response.json();
 
   return {
