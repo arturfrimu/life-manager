9ab1688d7d1b6e75a0c5652a05f0f28f837173da
Daniel-1017 <frimudaniel17@gmail.com>
2025-10-21 23:14:05 +0200

    Created workout interface

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -8,3 +8,14 @@ interface Exercise {
     created: string;
     updated: string;
 }
+
+interface Workout {
+    id: string;
+    userId: string;
+    name: string;
+    notes: string;
+    startedAt: string;
+    completedAt: string;
+    created: string;
+    updated: string;
+}
