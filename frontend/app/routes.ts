90e2ac389a03b3ef8bec69fe483e6632bddd0091
Daniel-1017 <frimudaniel17@gmail.com>
2025-10-21 23:41:57 +0200

    Created workout route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("sport/workouts", "routes/topics/sport.tsx")
+    route("sport/workouts", "routes/topics/sport.tsx"),
+    route("sport/workouts/:id", "routes/topics/workout.tsx")
 ] satisfies RouteConfig;
